eebd017f4f75f246b4b1a9f413ab77406c96c360
Cjtrankz <flyingapple55@gmail.com>
2023-02-12 04:54:56 -0500

    script for cooking food

---
+++ Assets/Casey Assets/Scripts/CookFood.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookFood : MonoBehaviour
+{
+    
+    Color col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Food")
+        {
+            col = other.GetComponent<Renderer>().material.color;
+            Vector4 newCol = new Color(col.r - 0.002f,col.g - 0.002f,col.b - 0.002f);
+            other.GetComponent<Renderer>().material.color = newCol;
+            col = newCol;
+        }
+    }
+}
